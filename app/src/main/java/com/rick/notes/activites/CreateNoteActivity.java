24fea44bbc2367af3f87852d69cf31f9ceb772c6
Rick Ren <rick_ren@askey.com>
2020-11-20 14:37:51 +0800

    Android Development | Notes App | Tutorial #3 | Create & Save Note | Android Studio https://www.youtube.com/watch?v=RWYXbOCoVYk&list=PLam6bY5NszYN6-a1wt7yRISWfmYPdkbMu&index=3

--- app/src/main/java/com/rick/notes/activites/CreateNoteActivity.java
+++ app/src/main/java/com/rick/notes/activites/CreateNoteActivity.java
@@ -2,13 +2,27 @@ package com.rick.notes.activites;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.rick.notes.R;
+import com.rick.notes.database.NotesDatabase;
+import com.rick.notes.entities.Note;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 public class CreateNoteActivity extends AppCompatActivity {
+    private EditText inputNoteTitle, inputNoteSubtitle, inputNoteText;
+    private TextView textDateTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +31,57 @@ public class CreateNoteActivity extends AppCompatActivity {
 
         ImageView imageBack = findViewById(R.id.imageBack);
         imageBack.setOnClickListener(v -> onBackPressed());
+
+        inputNoteTitle = findViewById(R.id.inputNoteTitle);
+        inputNoteSubtitle = findViewById(R.id.inputNoteSubtitle);
+        inputNoteText = findViewById(R.id.inputNoteText);
+
+        textDateTime = findViewById(R.id.textDateTime);
+        textDateTime.setText(new SimpleDateFormat(
+                "EEEE, dd MMMM yyyy HH:mm a", Locale.getDefault()).format(new Date().getTime())
+        );
+
+        ImageView imageSave = findViewById(R.id.imageSave);
+        imageSave.setOnClickListener(v -> saveNote());
+    }
+
+    private void saveNote() {
+        final String noteTitle = inputNoteTitle.getText().toString().trim();
+        final String noteSubtitle = inputNoteSubtitle.getText().toString().trim();
+        final String noteText = inputNoteText.getText().toString().trim();
+        final String dateTimeStr = textDateTime.getText().toString().trim();
+
+        if (noteTitle.isEmpty()) {
+            Toast.makeText(this, "Note title can't be empty!", Toast.LENGTH_SHORT).show();
+            return;
+        } else if (noteSubtitle.isEmpty() && noteText.isEmpty()) {
+            Toast.makeText(this, "Note can't be empty!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        final Note note = new Note();
+        note.setTitle(noteTitle);
+        note.setSubtitle(noteSubtitle);
+        note.setNoteText(noteText);
+        note.setDateTime(dateTimeStr);
+
+        @SuppressLint("StaticFieldLeak")
+        class SaveNoteTask extends AsyncTask<Void, Void, Void> {
+            @Override
+            protected Void doInBackground(Void... voids) {
+                NotesDatabase.getNotesDatabase(getApplicationContext()).noteDao().insertNote(note);
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(Void aVoid) {
+                super.onPostExecute(aVoid);
+                Intent intent = new Intent();
+                setResult(RESULT_OK, intent);
+                finish();
+            }
+        }
+
+        new SaveNoteTask().execute();
     }
 }
--- app/src/main/java/com/rick/notes/activites/MainActivity.java
+++ app/src/main/java/com/rick/notes/activites/MainActivity.java
@@ -1,15 +1,22 @@
 package com.rick.notes.activites;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ImageView;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.rick.notes.R;
-import com.rick.notes.activites.CreateNoteActivity;
+import com.rick.notes.database.NotesDatabase;
+import com.rick.notes.entities.Note;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = MainActivity.class.getSimpleName();
 
     public static final int REQUEST_CODE_ADD_NOTE = 1;
 
@@ -22,5 +29,28 @@ public class MainActivity extends AppCompatActivity {
         imageAddNoteMain.setOnClickListener(v -> startActivityForResult(new Intent(
                 getApplicationContext(), CreateNoteActivity.class), REQUEST_CODE_ADD_NOTE)
         );
+
+        getNotes();
+    }
+
+    private void getNotes() {
+
+        @SuppressLint("StaticFieldLeak")
+        class GetNoteTask extends AsyncTask<Void, Void, List<Note>> {
+
+            @Override
+            protected List<Note> doInBackground(Void... voids) {
+                return NotesDatabase.getNotesDatabase(getApplicationContext())
+                        .noteDao().getAllNotes();
+            }
+
+            @Override
+            protected void onPostExecute(List<Note> notes) {
+                super.onPostExecute(notes);
+                Log.d(TAG, "onPostExecute: " + notes.toString());
+            }
+        }
+
+        new GetNoteTask().execute();
     }
 }
